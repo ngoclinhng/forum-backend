b02999f66fe20cd925597cd011ef6d7056cabc72
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2022-05-24 23:20:47 +0700

    Test create_thread/1

--- test/yojee/forum_test.exs
+++ test/yojee/forum_test.exs
@@ -2,4 +2,38 @@ defmodule Yojee.ForumTest do
   use Yojee.DataCase, asyn: true
 
   alias Yojee.Forum
+  alias Yojee.Forum.Thread
+
+  describe "create_thread/1" do
+    test "with valid data inserts thread" do
+      assert {:ok, thread} = Forum.create_thread(%{title: "Test title"})
+      assert %Thread{} = thread
+      assert thread.title === "Test title"
+    end
+
+    test "with invalid data returns error changeset" do
+      assert {:error, changeset} = Forum.create_thread(%{title: nil})
+      assert %Ecto.Changeset{} = changeset
+    end
+
+    test "trims whitespaces on both sides of title" do
+      assert {:ok, thread} = Forum.create_thread(%{title: "  Test title "})
+      assert %Thread{} = thread
+      assert thread.title === "Test title"
+    end
+
+    test "requires title to have at least 3 characters long" do
+      assert {:error, changeset} = Forum.create_thread(%{title: "ab"})
+      assert %{title: ["should be at least 3 character(s)"]} =
+        errors_on(changeset)
+    end
+
+    test "requires title to have at most 140 characters long" do
+      s = String.duplicate("titl ", 28) <> "e"
+      assert String.length(s) === 141
+      assert {:error, changeset} = Forum.create_thread(%{title: s})
+      assert %{title: ["should be at most 140 character(s)"]} =
+        errors_on(changeset)
+    end
+  end
 end
