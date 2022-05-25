a72a84dd4974b4618e29b114800fbe3af9ab07f5
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2022-05-25 10:43:29 +0700

    Post changeset

--- lib/yojee/forum/post.ex
+++ lib/yojee/forum/post.ex
@@ -7,6 +7,9 @@ defmodule Yojee.Forum.Post do
   # behaviour by using `utc_datetime` instead.
   @timestamps_opts [type: :utc_datetime]
 
+  # The length of each post must be in the range [min, max].
+  @allowed_content_length_range [min: 1, max: 10_000]
+
   schema "posts" do
     field :content, :string
     belongs_to :thread, Yojee.Forum.Thread
@@ -16,8 +19,23 @@ defmodule Yojee.Forum.Post do
 
   @doc false
   def changeset(post, attrs) do
+    required_fields = [:content, :thread_id]
+    optional_fields = []
+
     post
-    |> cast(attrs, [:content])
-    |> validate_required([:content])
+    |> cast(attrs, required_fields ++ optional_fields)
+    |> validate_required(required_fields)
+    |> validate_content()
+    |> assoc_constraint(:thread)
   end
+
+  # Helpers
+
+  defp validate_content(%Ecto.Changeset{valid?: true} = changeset) do
+    changeset
+    |> update_change(:content, &String.trim/1)
+    |> validate_length(:content, @allowed_content_length_range)
+  end
+
+  defp validate_content(changeset), do: changeset
 end
