bf22919b7e5e024ffff8bd4a6c3ef5c0b01d9c17
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2022-05-25 17:24:07 +0700

    threads with posts fixture

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,11 +1,15 @@
 defmodule Yojee.Factory do
   alias Yojee.Repo
-  alias Yojee.Forum.Thread
+  alias Yojee.Forum.{Thread, Post}
 
   # Factories
 
   def build(:thread) do
     %Thread{title: "thread-#{System.unique_integer([:positive])}"}
+  end
+
+  def build(:post) do
+    %Post{content: "post-#{System.unique_integer([:positive])}"}
   end
 
   # Convenience API
@@ -20,5 +24,17 @@ defmodule Yojee.Factory do
     factory_name
     |> build(attributes)
     |> Repo.insert!()
+  end
+
+  def insert_thread_with_posts!(num_posts) do
+    posts = for _ <- 1..num_posts, do: build(:post)
+    build(:thread, posts: posts)
+    |> Repo.insert!
+  end
+
+  def threads_with_posts_fixture(num_threads \\ 5) do
+    0..num_threads
+    |> Enum.reverse()
+    |> Enum.map(&insert_thread_with_posts!/1)
   end
 end
